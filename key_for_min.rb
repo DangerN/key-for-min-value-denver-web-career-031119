5ff657e1bcc02254cc6990adcd84c60d755c33ea
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 20:35:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  r_key = ''
+  r_key = nil
   r_val = 100000
   name_hash.each do | key, value |
     if value < r_val
